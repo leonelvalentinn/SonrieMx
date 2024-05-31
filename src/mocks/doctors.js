37eb613a7567dc4d5bb6f0d3d7609219dbf6dbaf
leonelvalentinn <leonelvalentinn@gmail.com>
2024-05-31 17:06:30 -0600

    Changes

---
+++ src/mocks/doctors.js
@@ -0,0 +1,17 @@
+export const doctors = [
+  {
+    id: 1,
+    name: 'Dra. Mariana I. Argaez',
+    imgSrc: '/images/doctora.webp'
+  },
+  {
+    id: 2,
+    name: 'Dr. Louis D. Jasso',
+    imgSrc: '/images/doctor.webp'
+  },
+  {
+    id: 3,
+    name: 'Dr. John Doe',
+    imgSrc: '/images/doctor.webp'
+  }
+]
