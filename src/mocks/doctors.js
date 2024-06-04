6d90b591d57aa6acc03f550e3802b1a371a51067
leonelvalentinn <leonelvalentinn@gmail.com>
2024-06-04 11:29:03 -0600

    Add more doctors

--- src/mocks/doctors.js
+++ src/mocks/doctors.js
@@ -2,19 +2,49 @@ export const doctors = [
   {
     id: 1,
     name: 'Dra. Mariana Argaez',
-    imgSrc: '/images/doctora.webp',
-    specialty: ''
+    imgSrc: '/images/doctors/mariana.webp',
+    specialty: 'Odontología General y Armonización Facial'
   },
   {
     id: 2,
     name: 'Dr. Louis Jasso',
-    imgSrc: '/images/louis.webp',
-    specialty: ''
+    imgSrc: '/images/doctors/louis-2.webp',
+    specialty: 'Odontología General y Estética Dental'
   },
   {
     id: 3,
-    name: 'Dr. Jhon',
-    imgSrc: '/images/doctor.webp',
-    specialty: ''
+    name: 'Dra. Daniela Reyes',
+    imgSrc: '/images/doctors/daniela-reyes.webp',
+    specialty: 'Odontología General y Asistente Dental'
+  },
+  {
+    id: 4,
+    name: 'Dr. Héctor Robles',
+    imgSrc: '/images/doctors/hector-robles.webp',
+    specialty: 'Especialista en Prótesis Bucal e Implantología'
+  },
+  {
+    id: 5,
+    name: 'Dra. Ammy Solache',
+    imgSrc: '/images/doctors/ammy-solache.webp',
+    specialty: 'Especialista en Endodoncia'
+  },
+  {
+    id: 6,
+    name: 'Dra. Valeria Ceballos',
+    imgSrc: '/images/doctors/valeria-ceballos.webp',
+    specialty: 'Odontología General y Asistente Dental'
+  },
+  {
+    id: 7,
+    name: 'Dr. Giovanni Pérez',
+    imgSrc: '/images/doctors/giovanni-perez.webp',
+    specialty: 'Especialista en Endodoncia'
+  },
+  {
+    id: 8,
+    name: 'Dra. Daniela Lobato',
+    imgSrc: '/images/doctors/daniela-lobato.webp',
+    specialty: 'Especialista en Periodoncia'
   }
 ]
