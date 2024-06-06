e8ef1e0a932d6a3679361060d3f61a1f03baff2d
leonelvalentinn <leonelvalentinn@gmail.com>
2024-06-06 15:04:27 -0600

    Change Styles in smart´hone

--- src/mocks/services.js
+++ src/mocks/services.js
@@ -4,7 +4,7 @@ export const services = [
     title: 'Odontología Estética',
     description: 'Mejoramos la apariencia de tu sonrisa mediante el uso de carillas dentales. Logra una sonrisa armoniosa y radiante con nuestro cuidado experto.',
     icon: '/images/carillas-dentales.webp',
-    className: 'col-span-3'
+    className: 'md:col-span-3'
   },
   {
     id: 2,
@@ -12,47 +12,47 @@ export const services = [
     description: 'Realizamos limpiezas, extracciones y tratamientos preventivos para mantener tu salud bucal en óptimas condiciones. Confía en nosotros para cuidar de tu sonrisa.',
     icon: '/images/diente.webp',
     imgWidth: 'max-w-36 my-4',
-    className: 'col-span-3'
+    className: 'md:col-span-3'
   },
   {
     id: 3,
     title: 'Ortodoncia',
-    className: 'col-span-2'
+    className: 'md:col-span-2'
   },
   {
     id: 4,
     title: 'Cirugía Maxilofacial',
-    className: 'col-span-2'
+    className: 'md:col-span-2'
   },
   {
     id: 5,
     title: 'Endodoncia',
-    className: 'col-span-2'
+    className: 'md:col-span-2'
   },
   {
     id: 6,
     title: 'Periodoncia',
-    className: 'col-span-3'
+    className: 'md:col-span-3'
   },
   {
     id: 7,
     title: 'Implantología',
-    className: 'col-span-3'
+    className: 'md:col-span-3'
   },
   {
     id: 8,
     title: 'Prótesis',
-    className: 'col-span-2'
+    className: 'md:col-span-2'
   },
   {
     id: 9,
     title: 'Odontopediatría',
-    className: 'col-span-2'
+    className: 'md:col-span-2'
   },
   {
     id: 10,
     title: '¿Buscas algo especial?',
-    className: 'col-span-2',
+    className: 'md:col-span-2',
     special: 'true'
   }
 ]
