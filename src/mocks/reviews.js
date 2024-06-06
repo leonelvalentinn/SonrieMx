b303ff439945a0b06d1ce72937a1dcd4744658d8
leonelvalentinn <leonelvalentinn@gmail.com>
2024-06-06 17:21:57 -0600

    Add reviews

---
+++ src/mocks/reviews.js
@@ -0,0 +1,37 @@
+export const reviews = [
+  {
+    id: 1,
+    name: 'DANIELA AMARO VARGAS',
+    score: 5,
+    date: new Date(),
+    text: 'El Dr. Daniel hace un excelente trabajo, muy minucioso.'
+  },
+  {
+    id: 1,
+    name: 'Antonio Alejandro Jasso Mena',
+    score: 5,
+    date: new Date(),
+    text: 'Atención, calidad, humanidad, profesionalismo y experiencia. Es lo que vas a encontrar.'
+  },
+  {
+    id: 1,
+    name: 'arturo ramirez',
+    score: 5,
+    date: new Date(),
+    text: 'Muy buena atencion y servicio de la Dra. Mariana'
+  },
+  {
+    id: 1,
+    name: 'Carla Argaez',
+    score: 5,
+    date: new Date(),
+    text: 'Excelente servicio y profesionalismo, última tecnología en técnicas, diseño de sonrisa.  Precios accesibles y excelente ubicación'
+  },
+  {
+    id: 1,
+    name: 'karla vergara noriega',
+    score: 5,
+    date: new Date(),
+    text: 'Excelente servicio,no solo se enfocaron en el problema inicial, sino que también revisaron todos los aspectos de mi salud dental. La Dra Mariana mostró un alto nivel de profesionalismo, es muy detallista , lo cual me hizo sentir muy cómoda durante todo el procedimiento.'
+  },
+]
